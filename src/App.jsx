a033e85f12296671c466ab6b4dd3f0fbfe73432a
CJH <wlghdywjd@naver.com>
2025-02-13 19:34:01 +0900

    jiho test

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const slidesHero = [
   { src: "https://placehold.co/600x400/green/black", content: "Slide 3" },
   { src: "https://placehold.co/600x400/red/white", content: "Slide 4" },
   { src: "https://placehold.co/600x400/black/white", content: "Slide 5" },
+  { src: "https://placehold.co/600x400/black/white", content: "Slide 5 cjh" },
 ];
 
 const Section = styled.section`

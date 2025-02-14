af5f17d100cb70c5965ce77cd47acf9e58a7ab5a
cheol test <rkdguscjf111@naver.com>
2025-02-14 21:03:50 +0900

    button edit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,69 +1,110 @@
 // src/components/Button.js
 import React from "react";
 import styled from "styled-components";
-
 // FullButton 스타일 정의
-const FullButton = styled.a`
+export const HeroButton1 = styled.a`
   display: inline-block;
-  padding: 12px 24px;
+  padding: 14px 36px;
   font-family: ${({ theme }) => theme.fonts.main};
-  font-size: 16px;
-  font-weight: 700;
-  color: #ffffff;
+  font-size: ${({ theme }) => theme.fontSizes.fs18};
+  font-weight: 600;
+  color: #FFFFFF;
   background-color: ${({ theme }) => theme.colors.primary};
   border: none;
-  border-radius: 4px;
+  border-radius: 24px;
   cursor: pointer;
   text-align: center;
   text-decoration: none;
   transition: background-color 0.3s ease, color 0.3s ease;
-
   &:hover {
-    background-color: ${({ theme }) => theme.colors.primaryDark};
+    background-color: ${({ theme }) => theme.colors.primary600};
   }
 `;
-
-// OutlineButton 스타일 정의
-const OutlineButton = styled.a`
+export const HeroButton2 = styled.a`
   display: inline-block;
-  padding: 10px 22px;
+  padding: 14px 36px;
   font-family: ${({ theme }) => theme.fonts.main};
-  font-size: 16px;
-  font-weight: 700;
+  font-size: ${({ theme }) => theme.fontSizes.fs18};
+  font-weight: 600;
   color: ${({ theme }) => theme.colors.primary};
-  background-color: #ffffff;
-  border: 2px solid ${({ theme }) => theme.colors.primary};
-  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border: none;
+  border-radius: 24px;
   cursor: pointer;
   text-align: center;
   text-decoration: none;
-  box-sizing: border-box;
   transition: background-color 0.3s ease, color 0.3s ease;
-
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primary100};
+  }
+`;
+export const HeroButton3 = styled.a`
+  display: inline-block;
+  padding: 14px 36px;
+  font-family: ${({ theme }) => theme.fonts.main};
+  font-size: ${({ theme }) => theme.fontSizes.fs18};
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border: none;
+  border-radius: 24px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color 0.3s ease, color 0.3s ease;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.secondary700};
+  }
+`;
+// OutlineButton 스타일 정의
+export const OutlineButton = styled.a`
+  display: inline-block;
+  padding: 10px 36px;
+  font-family: ${({ theme }) => theme.fonts.main};
+  font-size: ${({ theme }) => theme.fontSizes.fs18};
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.primary200};
+  border-radius: 22px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color 0.3s ease, color 0.3s ease;
   &:hover {
     background-color: ${({ theme }) => theme.colors.primary};
-    color: #ffffff;
+    color: ${({ theme }) => theme.colors.white};
   }
 `;
-
 // Button 컴포넌트
 // full-btn
 const Button = ({ type, href, children }) => {
   switch (type) {
-    case "outline-btn":
-      return (
-        <OutlineButton href={href} target="_blank" rel="noopener noreferrer">
-          {children}
-        </OutlineButton>
-      );
-    case "full-btn":
+    case "hero-btn1":
     default:
       return (
-        <FullButton href={href} target="_blank" rel="noopener noreferrer">
+        <HeroButton1 href={href} target="_blank" rel="noopener noreferrer">
           {children}
-        </FullButton>
+        </HeroButton1>
       );
+      case "hero-btn2":
+      return (
+        <HeroButton2 href={href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </HeroButton2>
+      );
+      case "hero-btn3":
+        return (
+          <HeroButton3 href={href} target="_blank" rel="noopener noreferrer">
+            {children}
+          </HeroButton3>
+        );
+        case "outline-btn":
+          return (
+            <OutlineButton href={href} target="_blank" rel="noopener noreferrer">
+              {children}
+            </OutlineButton>
+          );
   }
 };
-
 export default Button;

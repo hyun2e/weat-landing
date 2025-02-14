2527fc9ec63764fdc9ee172fc144da04b5fc4652
cheol test <rkdguscjf111@naver.com>
2025-02-14 15:29:55 +0900

    ne

--- src/sections/Review.jsx
+++ src/sections/Review.jsx
@@ -3,92 +3,141 @@ import React from 'react'
 function Review() {
   return (
     <div>
-    <div>사용자들과 생생하고 신뢰도 높은 리뷰를 공유해보세요</div>
-    <div>
-    <div>
-        <div>
-            <div>다이어트</div>
-            <div>비건 채식</div>
-            <div>25.01.14</div>
-        </div>
-        <div>
-            <div>퍼맨츠 가지요리 너무 존맛</div>
-            <div>채식주의 식단을 하고 있지는 않지만, 비건이 아닌 음식들과 비교해도 맛있어요! 친구와 신년모임으로 오랜만에 만났는데 요즘 소화가 잘 안됐는데 ···</div>
-        </div>
-        <div>
+        <div>사용자들과 생생하고 신뢰도 높은 리뷰를 공유해보세요</div>
+        <div> 
+         <div> 
+            <div> 
+                <div>
+                    <div>다이어트</div>
+                    <div>비건 채식</div>
+                    <div>25.01.14</div>
+                </div>
+                <div>
+                    <div>퍼맨츠 가지요리 너무 존맛</div>
+                    <div>채식주의 식단을 하고 있지는 않지만, 비건이 아닌 음식들과 비교해도 맛있어요! 친구와 신년모임으로 오랜만에 만났는데 요즘 소화가 잘 안됐는데 ···</div>
+                </div>
+                <div>
+                    <div>
+                        <img/>
+                        <div>36</div>
+                    </div>
+
+                    <div>
+                        <img/>
+                        <div>11</div>
+                    </div>
+                        
+                    <div>
+                        <img/>
+                    </div>
+                </div>
+            </div>
+            
             <div>
                 <img/>
-                <div>36</div>
+            </div>
+        </div>
+
+        <div> 
+            <div> 
+                <div>
+                    <div>저탄고지지</div>
+                    <div>다이어트트</div>
+                    <div>25.12.19</div>
+                </div>
+                <div>
+                    <div>요거트 볼 맛집 플러디 강추</div>
+                    <div>요거트는 담백하고 건강한 맛이라 자주 찾는 편인데, 여기 요거트는 진짜 최고였어요! 🥄 처음 한입 먹자마자 부드러움과 은은한 단맛이 퍼지는 게 ···</div>
+                </div>
+                <div>
+                    <div>
+                        <img/>
+                        <div>0</div>
+                    </div>
+
+                    <div>
+                        <img/>
+                        <div>18</div>
+                    </div>
+                        
+                    <div>
+                        <img/>
+                    </div>
+                </div>
+            </div>
+            
+            <div>
+                <img/>
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <div>저속노화</div>
+                    <div>비건 채식</div>
+                    <div>25.01.14</div>
+                </div>
+                <div>
+                    <div>포케맛집 ㅇㅈ</div>
+                    <div>요즘 속이 좀 예민해서 가벼운 음식이 땡겼는데, 포케가 딱이었어요! 신선한 재료들이 가득해서 한입 한입 먹을 때마다 상쾌한 느낌</div>
+                </div>
+                <div>
+                    <div>
+                        <img/>
+                        <div>9</div>
+                    </div>
+
+                    <div>
+                        <img/>
+                        <div>11</div>
+                    </div>
+                        
+                    <div>
+                        <img/>
+                    </div>
+                </div>
             </div>
 
             <div>
                 <img/>
-                <div>11</div>
             </div>
+        </div>
+
+        <div>
+            <div>
+                <div>
+                    <div>고단백</div>
+                    <div>25.01.26</div>
+                </div>
+                <div>
+                    <div>마파 두부 알싸한 맛</div>
+                    <div>평소 마파두부를 좋아하는 편이라 기대하면서 방문했는데, 기대 이상이었어요! 비건은 아니지만, 가끔 두부 요리를 찾곤 하는데, 여기 마파두부는  ···</div>
+                </div>
+                <div>
+                    <div>
+                        <img/>
+                        <div>21</div>
+                    </div>
+
+                    <div>
+                        <img/>
+                        <div>11</div>
+                    </div>
+                        
+                    <div>
+                        <img/>
+                    </div>
+                </div>
+            </div>
+
             <div>
                 <img/>
             </div>
         </div>
-        <div><img/></div>
     </div>
-
-    <div>
-        <div>
-            <div>저탄고지</div>
-            <div>다이어트</div>
-            <div>25.01.14</div>
-        </div>
-        <div>요거트 볼 맛집 플러디 강추</div>
-        <div>요거트는 담백하고 건강한 맛이라 자주 찾는 편인데, 여기 요거트는 진짜 최고였어요! 🥄 처음 한입 먹자마자 부드러움과 은은한 단맛이 퍼지는 게 ···</div>
-        <div>
-            <img/>
-            <div>0</div>
-            <img/>
-            <div>18</div>
-            <img/>
-        </div>
-        <div><img/></div>
-    </div>
-
-    <div>
-        <div>
-            <div>저속노화</div>
-            <div>비건 채식</div>
-            <div>25.01.14</div>
-        </div>
-        <div>포케 맛집 ㅇㅈ</div>
-        <div>요즘 속이 좀 예민해서 가벼운 음식이 땡겼는데, 포케가 딱이었어요! 신선한 재료들이 가득해서 한입 한입 먹을 때마다 상쾌한 느낌</div>
-        <div>
-            <img/>
-            <div>9</div>
-            <img/>
-            <div>11</div>
-            <img/>
-        </div>
-        <div><img/></div>
-    </div>
-
-    <div>
-        <div>
-            <div>고단백백</div>
-            <div>25.01.14</div>
-        </div>
-
-        <div>마파 두부 알싸한 맛</div>
-        <div>평소 마파두부를 좋아하는 편이라 기대하면서 방문했는데, 기대 이상이었어요! 비건은 아니지만, 가끔 두부 요리를 찾곤 하는데, 여기 마파두부는  ···</div>
-        <div>
-            <img/>
-            <div>21</div>
-            <img/>
-            <div>11</div>
-            <img/>
-        </div>
-        <div><img/></div>
-    </div>
-    </div>
-    
-<button> 리뷰 더 보러가기</button>
-    </div>
+    <button> 리뷰 더 보러가기</button>
+</div>
   )
 }
 

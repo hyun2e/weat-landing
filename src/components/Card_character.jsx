8dd76587da9b80f8410baded1a80ca5848c969d2
cheol test <rkdguscjf111@naver.com>
2025-02-17 11:37:14 +0900

    cheol - card character

--- src/components/Card_character.jsx
+++ src/components/Card_character.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
-const Card_character = () => {
-  return <div>Card_character</div>;
+const Card_character = ({ text1, text2 ,icons, plant}) => {
+
+  console.log(text1);
+  console.log(text2);
+  console.log(plant);
+  console.log(icons);
+
+  const plantStyle = {
+    backgroundColor:"#FFF7F2",
+    padding: "4px",
+    borderRadius: "9px",
+    display: "inline-block",
+    height: "61px",
+    width: "61px",
+    display: "flex",
+    justifyContent: "center", 
+    alignItems: "center",
+  };
+  
+  return (
+    <div>
+       <div style={plantStyle}> <img src={plant} /> </div>
+        <div>
+            <div>
+              <p>{text1}</p>
+                <div>
+          {/* 아이콘 배열을 순회하여 각 아이콘을 렌더링 */}
+          {icons.map((icon, index) => (
+            <img key={index} src={icon} alt={`icon-${index}`} />
+          ))}
+               </div>
+            </div>
+            <p>{text2}</p>
+        </div>
+    </div>
+  );
 };
 
 export default Card_character;
--- src/sections/Character.jsx
+++ src/sections/Character.jsx
@@ -1,99 +1,83 @@
 import React from 'react'
 import Button from '../components/Button'
+import Card_character from '../components/Card_character'
 
 const character = () => {
   return (
     <div>
+        
         <div>
             <h2>포인트를 모아서 4종 랜덤 위펫을 만나보세요!</h2>
             <p>포인트를 모으면 화분에서 꽃이 자라고,랜덤 위펫 등장해요! 🌱🍅🥕🥦 어떤 친구가 나올지 기대해 보세요!</p>
         </div>
 
         <div>
-            <div>
-                <img/>
-                <div>
-                    <div>
-                        <div>Lv.1</div>
-                        <div>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                        </div>
-                    </div>
-                    <p>작은 씨앗에서 새로운 생명의 시작을 느껴보세요!</p>
-                </div>
-            </div>
+            <Card_character
+            plant="src/assets/images/Flower_Level1.svg"
+            text1="Lv.1"
+            icons={[
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart grey.svg",
+                "src/assets/images/icon_Heart grey.svg",
+                "src/assets/images/icon_Heart grey.svg",
+                "src/assets/images/icon_Heart grey.svg",    
+              ]}
+            text2="작은 씨앗에서 새로운 생명의 시작을 느껴보세요!"
+            />
 
-            <div>
-                <img/>
-                <div>
-                    <div>
-                        <div>Lv.2</div>
-                        <div>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                        </div>
-                    </div>
-                    <p>첫 싹이 틀 때, 당신의 식물도 함께 자라기 시작합니다!</p>
-                </div>
-            </div>
+            <Card_character
+              plant="src/assets/images/Flower_Level2.svg"
+              text1="Lv.2"
+               icons={[
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart grey.svg",
+                "src/assets/images/icon_Heart grey.svg",
+                "src/assets/images/icon_Heart grey.svg",    
+                 ]}
+               text2="첫 싹이 틀 때, 당신의 식물도 함께 자라기 시작합니다!"
+                 />
 
-            <div>
-                <img/>
-                <div>
-                    <div>
-                        <div>Lv.3</div>
-                        <div>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                        </div>
-                    </div>
-                    <p>싹에서 꽃 봉우리가 싹트기 시작했습니다!</p>
-                </div>
-            </div>
+            <Card_character
+              plant="src/assets/images/Flower_Level3.svg"
+              text1="Lv.3"
+               icons={[
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart grey.svg",
+                "src/assets/images/icon_Heart grey.svg",    
+                 ]}
+               text2="싹에서 꽃 봉우리가 싹트기 시작했습니다!"
+                />
 
-            <div>
-                <img/>
-                <div>
-                    <div>
-                        <div>Lv.4</div>
-                        <div>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                        </div>
-                    </div>
-                    <p>아름다운 꽃이 만개할 순간이 가까워졌습니다!</p>
-                </div>
-            </div>
+            <Card_character
+              plant="src/assets/images/Flower_Level4.svg"
+              text1="Lv.4"
+               icons={[
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart grey.svg",    
+                 ]}
+               text2="아름다운 꽃이 만개할 순간이 가까워졌습니다!"
+                />
 
-            <div>
-                <img/>
-                <div>
-                    <div>
-                        <div>Lv.5</div>
-                        <div>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                        </div>
-                    </div>
-                    <p>아름다운 꽃이 어떤 열매를 맺었을까요?</p>
-                </div>
-            </div>
+            <Card_character
+              plant="src/assets/images/Flower_riddle.svg"
+              text1="Lv.5"
+               icons={[
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart.svg",
+                "src/assets/images/icon_Heart grey.svg",    
+                 ]}
+               text2="아름다운 꽃이 어떤 열매를 맺었을까요?"
+                />
+
+
         </div>
 
         <div>

327cd1e8430f464a433ea610302aff43489b7e3d
soim <ssoim322@gmail.com>
2025-02-17 12:07:21 +0900

    soim - card edit 2

--- src/components/Card_onboarding.jsx
+++ src/components/Card_onboarding.jsx
@@ -13,13 +13,20 @@ const OnboardingTextWarpper = styled.div`
   flex-direction: column;
   align-items: center;
   text-align: center;
+  gap:20px;
 `;
 
+const OnboardingCardImage = styled.img`
+  width: 335px;
+  height: 200px;
+  border-radius: 16px;
+  border: none;
+`
 
 const Card_onboarding = ({icon,title,text}) => {
   return (
     <OnboardingCard>
-      <img src={icon} />
+      <OnboardingCardImage src={icon} />
       <OnboardingTextWarpper>
         <h4>{title}</h4>
         <p>{text}</p>
--- src/sections/Download.jsx
+++ src/sections/Download.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const DownloadWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap:30px;
+    padding: 0 16px 52px 16px;
+`
+
 const DownloadButton = styled.button`
     display: flex;
     align-items: center;
@@ -17,18 +27,25 @@ const DownloadButtonWrapper = styled.div`
     justify-content: center;
     gap: 12px;
 `
+const DownloadTextWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap:12px;
+`
 
 const download = () => {
   return (
-    <div>
-        <div>
+    <DownloadWrapper>
+        <DownloadTextWrapper>
             <h2>
             위잇트와 함께해요!
             </h2>
             <p>
             건강한 한 끼가 더 쉬워지는 곳, 위잇트 새로운 건강식 경험을 만들어갈 여러분을 기다립니다.
             </p>
-        </div>
+        </DownloadTextWrapper>
         <DownloadButtonWrapper>
             <DownloadButton>
                 <img src="src/assets/images/PC_google play.png" alt="구글 플레이 스토어"/>
@@ -37,7 +54,7 @@ const download = () => {
                 <img src="src/assets/images/PC_app store.png" alt="앱 스토어"/>
             </DownloadButton>
         </DownloadButtonWrapper>
-    </div>
+    </DownloadWrapper>
   )
 }
 

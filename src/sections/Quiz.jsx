e01ab4e58490e5f305df0171f8921f20f3ff26fb
cheol test <rkdguscjf111@naver.com>
2025-02-18 09:02:57 +0900

    quiz  수정정

--- src/sections/Quiz.jsx
+++ src/sections/Quiz.jsx
@@ -15,6 +15,8 @@ const Wrapper = styled.div`
 
 // quizWrapper를 div로 변경
 const QuizWrapper = styled.div`
+  width: 100%;
+  height: 275px;
   gap: 30px;
   padding: 40px 16px 20px 16px;
   margin: auto;
@@ -27,20 +29,15 @@ const QuizWrapper = styled.div`
 
 // quizcardWrapper를 div로 변경
 const QuizCardWrapper = styled.div`
-  margin: auto;
-  display: grid;
-  grid-template-columns: repeat(3, 1fr); /* 3개의 이미지를 가로로 나열 */
+  width: 324px;
+  height: 200px; /* 이미지 크기에 맞는 높이 설정 */
+  display: flex;
+  flex-direction: row;
   align-items: center;
-  justify-content: center; /* 가로 가운데 정렬 */
-  text-align: center;
+  justify-content: center;
+  position: relative; /* 상대 위치 지정 */
 `;
 
-const Texth2 = styled.a`
-  color: ${({ theme }) => theme.colors.grey900};
-  font-size: ${({ theme }) => theme.fontSizes.fs28};
-  font-weight: 700;
-  text-align: center;
-`;
 
 const TextP = styled.a`
   color: ${({ theme }) => theme.colors.grey600};
@@ -50,7 +47,7 @@ const TextP = styled.a`
 `;
 
 const TextWrapper = styled.div`
-  margin: auto;
+  width: 324px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -64,14 +61,40 @@ const Quiz = () => {
     <Wrapper>
       <QuizWrapper>
         <QuizCardWrapper>
-          <img src="src/assets/images/quizcard_1.svg" alt="spelling" />
-          <img src="src/assets/images/quizcard_3.svg" alt="OX" />
-          <img src="src/assets/images/quizcard_2.svg" alt="picture" />
+        <img
+  src="src/assets/images/quizcard_1.svg"
+  alt="spelling"
+  style={{
+    position: 'absolute',
+    right: '70px', // 첫 번째 이미지 (스펠링), 살짝 왼쪽으로 배치
+    zIndex: 0, // 뒤쪽으로 배치
+  }}
+/>
+<img
+  src="src/assets/images/quizcard_3.svg"
+  alt="OX"
+  style={{
+    position: 'absolute',
+    left: '50%', // OX 이미지를 가로 중앙에 배치
+    transform: 'translateX(-50%)', // 중앙 정렬을 위해 이동
+    zIndex: 2, // 가장 앞에 배치
+  }}
+/>
+<img
+  src="src/assets/images/quizcard_2.svg"
+  alt="picture"
+  style={{
+    position: 'absolute',
+    left: '70px', // 세 번째 이미지 (사진), 살짝 오른쪽으로 배치
+    zIndex: 1, // 뒤쪽에 배치
+  }}
+/>
+
         </QuizCardWrapper>
         <TextWrapper>
-          <Texth2>
+          <h2>
             퀴즈와 출석체크를 <br /> 통해서 포인트를 얻어보세요
-          </Texth2>
+          </h2>
           <TextP>
             퀴즈를 풀고 출석 체크하면 포인트가 쌓여요!<br /> 건강도 챙기고 혜택도 받아보세요.
           </TextP>

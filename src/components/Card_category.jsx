ef93fc22e1d59f34cd23b1e8db305a4d2515e7f8
gahyun <gahyun94526@gmail.com>
2025-02-20 16:14:35 +0900

    카테고리 props 수정 & a 태그 중첩 네스팅 제거

--- src/components/Card_category.jsx
+++ src/components/Card_category.jsx
@@ -8,22 +8,22 @@ const CategoryCard = styled.div`
   align-items: center;
   text-align: center;
   width: 80px;
-  height:107px;
+  height: 107px;
   gap: 6px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 60px;
     height: 82px;
-  };
+  }
 `;
 
 // 카테고리 카트 이미지 스타일
 const CategoryCardImage = styled.img`
- @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     width: 60px;
     height: 60px;
   }
-`
+`;
 
 // 카테고리 카드 텍스트 스타일
 const CategoryCardText = styled.p`
@@ -36,11 +36,11 @@ const CategoryCardText = styled.p`
   }
 `;
 
-const Card_category = ({ icon, text }) => {
+const Card_category = ({ $icon, $text }) => {
   return (
     <CategoryCard>
-      <CategoryCardImage src={icon} alt={text} />
-      <CategoryCardText>{text}</CategoryCardText>
+      <CategoryCardImage src={$icon} alt={$text} />
+      <CategoryCardText>{$text}</CategoryCardText>
     </CategoryCard>
   );
 };
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const FooterContainer = styled.div`
   width: 100vw;
@@ -13,41 +13,41 @@ const FooterContainer = styled.div`
   transform: translateX(-50%);
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-        padding: 30px 20px;
+    padding: 30px 20px;
   }
-`
+`;
 const FooterContents = styled.div`
   width: 800px;
-  max-width: none; 
+  max-width: none;
   display: flex;
   flex-direction: row;
   align-items: top;
   justify-content: space-between;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-        flex-direction: column;
-        gap: 20px;
+    flex-direction: column;
+    gap: 20px;
   }
-`
+`;
 const FooterTextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: left;
   gap: 16px;
-`
+`;
 // 푸터 탭 스타일
 const FooterTabWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: 14px;
-`
+`;
 const FooterTab = styled.a`
   color: ${({ theme }) => theme.colors.grey100};
   font-size: ${({ theme }) => theme.fontSizes.fs16};
   font-weight: 600;
   line-height: 1.5;
   text-decoration: none;
-  
+
   &:hover {
     color: ${({ theme }) => theme.colors.secondary};
   }
@@ -61,7 +61,7 @@ const FooterTab = styled.a`
 const FooterDetailWrapper = styled.div`
   display: flex;
   flex-direction: column;
-`
+`;
 const FooterDetail = styled.a`
   display: flex;
   flex-direction: row;
@@ -69,22 +69,23 @@ const FooterDetail = styled.a`
   color: ${({ theme }) => theme.colors.grey250};
   font-size: ${({ theme }) => theme.fontSizes.fs14};
   line-height: 1.5;
+  text-decoration: none; /* 밑줄 제거 */
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     font-size: ${({ theme }) => theme.fontSizes.fs11};
   }
-`
+`;
 
 const FooterIconWrapper = styled.div`
   display: flex;
   flex-direction: row;
   gap: 8px;
-`
+`;
 const FooterIconButton = styled.a`
   align-items: center;
   justify-content: center;
   background-color: none;
-`
+`;
 const FooterIconimg = styled.img`
   width: 100%;
   height: 100%;
@@ -93,8 +94,7 @@ const FooterIconimg = styled.img`
     width: 24px;
     height: 24px;
   }
-`
-
+`;
 
 const Footer = () => {
   return (
@@ -104,37 +104,47 @@ const Footer = () => {
           <FooterTabWrapper>
             <FooterTab href="https://www.naver.com/" target="_blank">
               개인정보처리방침
-            </FooterTab> 
+            </FooterTab>
             <FooterTab>| </FooterTab>
             <FooterTab href="https://www.naver.com/" target="_blank">
-            서비스 이용약관
+              서비스 이용약관
             </FooterTab>
           </FooterTabWrapper>
 
           <FooterDetailWrapper>
             <FooterDetail>주식회사 일인팁삼프제</FooterDetail>
             <FooterDetail>대표 : 강현철, 배고은, 정소임, 최지호</FooterDetail>
-            <FooterDetail>주소 : 서울특별시 강남구 테헤란로44길 8, 12-13층</FooterDetail>
             <FooterDetail>
-              이메일 : 
-              <FooterDetail href="mailto:weat@tpfj.co.kr" target="blank">
-                  help@weat.co.kr
-              </FooterDetail>
+              주소 : 서울특별시 강남구 테헤란로44길 8, 12-13층
+            </FooterDetail>
+            <FooterDetail href="mailto:weat@tpfj.co.kr" target="blank">
+              이메일 : help@weat.co.kr
             </FooterDetail>
             <FooterDetail>업자등록번호 : 576-97-02738</FooterDetail>
-            <FooterDetail>ⓒ 2025. 일인팁삼프제 Co., Ltd. All rights reserved.</FooterDetail>
+            <FooterDetail>
+              ⓒ 2025. 일인팁삼프제 Co., Ltd. All rights reserved.
+            </FooterDetail>
           </FooterDetailWrapper>
         </FooterTextWrapper>
 
         <FooterIconWrapper>
           <FooterIconButton href="https://www.youtube.com" target="_blank">
-            <FooterIconimg src="src/assets/images/icon_youtube.svg" alt="YouTube" />
+            <FooterIconimg
+              src="src/assets/images/icon_youtube.svg"
+              alt="YouTube"
+            />
           </FooterIconButton>
           <FooterIconButton href="https://www.instagram.com" target="_blank">
-            <FooterIconimg src="src/assets/images/icon_insta.svg" alt="Instagram" />
+            <FooterIconimg
+              src="src/assets/images/icon_insta.svg"
+              alt="Instagram"
+            />
           </FooterIconButton>
           <FooterIconButton>
-            <FooterIconimg src="src/assets/images/icon_airplane.svg" alt="Message" />
+            <FooterIconimg
+              src="src/assets/images/icon_airplane.svg"
+              alt="Message"
+            />
           </FooterIconButton>
         </FooterIconWrapper>
       </FooterContents>
